39a21e1b324ffd188384dd873e1dfef1de7b2986
Mario Alberto Flores Nieves <mario.afn84@gmail.com>
2025-07-11 17:01:17 -0600

    modifique mi archivo

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('¡Backend funcionando desde Render!');
+  res.send('¡Mi Practica con Backend funcionando desde Render !');
 });
 
 app.listen(PORT, () => {
